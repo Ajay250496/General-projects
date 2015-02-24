5a4afe801b52a855a0c413c8665febc9ee479fd4
Ajay Jiwanand <ajay250496@live.com>
2015-02-24 11:32:07 -0500

    lol

--- Nmapscript.sh
+++ Nmapscript.sh
@@ -3,44 +3,99 @@
 
 ListScan()
 {
-	read $answer
-	echo $answer
-	if [ "$answer" == "1" ]; then
-		
-		if [ -a 192.168.1.0-Host.xml ]; then
-			mv 192.168.1.0-Host.txt 192.168.1.0-Host.txt.old
-			nmap -sP 192.168.1.0/24 | grep -oP '192.{0,10}'  > 192.168.1.0-Hosts.txt
+	if [ $answer == 1 ]; then
+		if [ -a 192.168.1.0-Hosts.xml ]; then
+			mv 192.168.1.0-Hosts.xml 192.168.1.0-Hosts.xml.old
+			nmap -sP 192.168.1.0/24 -oX 192.168.1.0-Hosts.xml > /dev/null 2>&1
+			echo "Difference in Hosts from the last time: "
+			ndiff 192.168.1.0-Hosts.xml 192.168.1.0-Hosts.xml.old 
 		else
-			#nmap -sP 192.168.1.0/24 -oX 192.168.1.0-Host.xml > /dev/null 2>&1
-			echo "nmap -sP 192.168.1.0/24" 
+			nmap -sP 192.168.1.0/24 -oX 192.168.1.0-Hosts.xml > /dev/null 2>&1
 		fi
 	fi
-	if [[ $answer == 2 ]]; then
-		if [ -a 192.168.2.0-Host.xml ]; then
-			mv 192.168.2.0-Host.txt 192.168.2.0-Host.txt.old
-			nmap -sP 192.168.2.0/24 | grep -oP '192.{0,10}' > 192.168.2.0-Host.txt
+	if [ $answer == 2 ]; then
+		if [ -a 192.168.2.0-Hosts.xml ]; then
+			mv 192.168.2.0-Hosts.xml 192.168.2.0-Hosts.xml.old
+			nmap -sP 192.168.2.0/24 -oX 192.168.2.0-Hosts.xml > /dev/null 2>&1
+			echo "Difference in Hosts from the last time: "
+			ndiff 192.168.2.0-Hosts.xml 192.168.2.0-Hosts.xml.old 
 		else
-			nmap -sP 192.168.2.0/24 | grep -oP '192.{0,10}' > 192.168.2.0-Host.txt
+			nmap -sP 192.168.2.0/24 -oX 192.168.2.0-Hosts.xml > /dev/null 2>&1
 		fi
 	fi
-	if [[ $answer == 2 ]]; then
-		if [ -a 30.0.1.0-Host.xml ]; then
-			mv 30.0.1.0-Host.xml 30.0.1.0-Host.xml.old
-			nmap  -sP 30.0.1.0/26 | grep -oP '30.{0}.{1}.{0,10}' > 30.0.1.0-Host.txt
+	if [ $answer == 3 ]; then
+		if [ -a 30.0.1.0-Hosts.xml ]; then
+			mv 30.0.1.0-Hosts.xml 30.0.1.0-Hosts.xml.old
+			nmap -sP 30.0.1.0/26 -oX 30.0.1.0-Hosts.xml > /dev/null 2>&1
+			echo "Difference in Hosts from the last time: "
+			ndiff 30.0.1.0-Hosts.xml 30.0.1.0-Hosts.xml.old 
 		else
-			nmap  -sP 30.0.1.0/26 | grep -oP '30.{0}.{1}.{0,10}' > 30.0.1.0-Host.txt
+			nmap -sP 30.0.1.0/26 -oX 30.0.1.0-Hosts.xml > /dev/null 2>&1
 		fi
 	fi
 }
 
- 
-echo "Which network would you like to run?
+OptionScan()
+{
+	if [ $Option == 1 ]; then
+		Option="-O"
+	elif [ $Option == 2 ]; then
+		Option="-sV"
+	fi
+	if [ $answer == 1 ];then
+		cat 192.168.1.0-Hosts.xml | grep -oP '192.{0,10}'  > 192.168.1.0-Hosts.txt
+		echo "Running $Option scan on 192.168.1.0/24"
+		if [ -a 192.168.1.0-Option.xml ]; then
+			mv 192.168.1.0-Option.xml 192.168.1.0-Option.xml.old
+			nmap $Option -iL 192.168.1.0-Hosts.txt -oX 192.168.1.0-Option.xml > /dev/null 2>&1
+			echo "Scan completed scan file created"			
+			echo "Difference in scan from last time: "
+			ndiff 192.168.1.0-Option.xml 192.168.1.0-Option.xml.old
+		else
+			nmap $Option -iL 192.168.1.0-Hosts.txt -oX 192.168.1.0-Option.xml
+		fi
+	fi
+	if [ $answer == 2 ];then
+		cat 192.168.2.0-Hosts.xml | grep -oP '192.{0,10}'  > 192.168.2.0-Hosts.txt
+		echo "Running $Option scan on 192.168.2.0/24"
+		if [ -a 192.168.2.0-Option.xml ]; then
+			mv 192.168.2.0-Option.xml 192.168.2.0-Option.xml.old
+			nmap $Option -iL 192.168.2.0-Hosts.txt -oX 192.168.2.0-Option.xml > /dev/null 2>&1
+			echo "Scan completed scan file created"
+			echo "Difference in scan from last time: "
+			ndiff 192.168.2.0-Option.xml 192.168.2.0-Option.xml.old > ndiff-192.168.2.0.txt
+		else
+			nmap $Option -iL 192.168.2.0-Hosts.txt -oX 192.168.2.0-Option.xml
+		fi
+	fi
+	if [ $answer == 3 ];then
+	cat 30.0.1.0-Hosts.xml | grep -oP '30.0.1.{0,10}'  > 30.0.1.0-Hosts.txt
+	echo "Running $Option scan on 30.0.1.0/26"
+		if [ -a 30.0.1.0-Option.xml ]; then
+			mv 30.0.1.0-Option.xml 30.0.1.0-Option.xml.old
+			nmap $Option -iL 30.0.1.0-Hosts.txt -oX 30.0.1.0-Option.xml > /dev/null 2>&1
+			echo "Scan completed scan file created"
+			echo "Difference in scan from last time: "
+			ndiff 30.0.1.0-Option.xml 30.0.1.0-Option.xml.old > ndiff-30.0.1.0.txt
+		else
+			nmap $Option -iL 30.0.1.0-Hosts.txt -oX 30.0.1.0-Option.xml
+		fi
+	fi
+}			
+
+
+echo -e "Which network would you like to run?
 1) 192.168.1.0
 2) 192.168.2.0
 3) 30.0.1.0
-	"
-#if [ $answer == 1 ]; then
-#	echo "Answer == 1"
-#fi
+Selection: \c"
+read answer
+echo -e "Which options would you like to use?
+1) OS Scan (-O)
+2) Version Detection Scan (-sV)
+3) Other (Please enter command line argument only e.g -A, -v, etc)
+Selection: \c"
+read Option
 
 ListScan
+OptionScan
